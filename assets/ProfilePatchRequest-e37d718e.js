e4743a296f69fd63ea572205daa1418479a417b2
Chandan Gowda <122788492+Chandangowda06@users.noreply.github.com>
2024-02-03 21:13:55 +0530

    Add files via upload

---
+++ assets/ProfilePatchRequest-e37d718e.js
@@ -0,0 +1,1 @@
+import{a as t,o as e,R as i}from"./index-faffc295.js";const c=async(a,o)=>{try{await t.patch(`${e}${o}/`,a,{...i()}),window.location.reload()}catch(r){window.alert(`Error updating Profile: ${r}`)}};export{c as u};
