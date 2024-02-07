1a60be263dacdc876f562f5baf7370a99b09b0f2
Chandan Gowda <122788492+Chandangowda06@users.noreply.github.com>
2024-02-07 22:55:52 +0530

    Delete assets directory

--- assets/ProfilePatchRequest-a15b5db5.js
+++
@@ -1,1 +0,0 @@
-import{a as s,o as i,R as c}from"./index-fe9762f0.js";const d=async(a,r,o,t)=>{try{await s.patch(`${i}${r}/`,a,{...c()}),o(t)}catch(e){window.alert(`Error updating Profile: ${e}`)}};export{d as u};
